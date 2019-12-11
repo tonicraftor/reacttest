417d3cfa75691348be579a67bb45e6ebc04a855e
tony luo <tonicraftor@gmail.com>
2019-12-11 17:58:06 -0600

    Updates

---
+++ precache-manifest.10b1ecade98a9032166480b2a2db09cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bdca9a36ea3b08ece8587d1a14673c",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "2c769ff71641aa971b60",
+    "url": "/reacttest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a5468da2607e2c5a8857",
+    "url": "/reacttest/static/js/2.035010c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reacttest/static/js/2.035010c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c769ff71641aa971b60",
+    "url": "/reacttest/static/js/main.ed12e39e.chunk.js"
+  },
+  {
+    "revision": "8c3f4207a2dd70ba0180",
+    "url": "/reacttest/static/js/runtime-main.8247b91e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
